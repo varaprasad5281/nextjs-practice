6d98225719b827c57c8cd2005284b589421c7f19
prasadgrid5281 <vkanugula@griddynamics.com>
2024-12-30 21:02:34 +0530

    first commit

---
+++ app/dashboard/sidenav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <div>
+        Sidebar
+    </div>
+  )
+}
+
+export default SideNav
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import {inter} from "@/app/font"
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         {children}
       </body>
